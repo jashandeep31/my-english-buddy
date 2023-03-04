bbf956648efb987c84497bdee7d8305199fba01a
Jashandeep Singh <jashandeep1659@gmail.com>
2023-03-04 20:32:34 +0530

    almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,57 @@
 const Header = document.querySelector("header");
-const Hero = document.querySelector("#hero");
+const Hero = document.querySelector("#home");
 const InnerWidth = window.innerWidth;
 const InnerHeight = window.innerHeight;
-
+const currentURL = window.location;
 let commentsNumber = 1;
 let added = false;
+
+const linksClasses = [
+    { homeLink: "home" },
+    { aboutLink: "aboutUs" },
+    { courseStrutureLink: "courseStructure" },
+    { faqLink: "FAQ" },
+    { contactLink: "contact" },
+];
+const linkActivator = () => {
+    let found = false;
+    linksClasses.forEach((link) => {
+        for (const key in link) {
+            if (currentURL.hash.includes(link[key])) {
+                document.querySelector(`.${key}`).classList.add("active");
+                found = true;
+            } else {
+                document.querySelector(`.${key}`).classList.remove("active");
+            }
+        }
+    });
+    if (!found) {
+        document.querySelector(`.home`).classList.add("active");
+    }
+};
+window.addEventListener("hashchange", function () {
+    linkActivator();
+});
+
 window.onload = function () {
     const HeaderHeight = Header.offsetHeight;
     Hero.style.paddingTop = HeaderHeight + "px";
-    if (InnerWidth < 768) {
+    linkActivator();
+
+    if (InnerWidth > 768) {
         commentsNumber = 2;
     }
-};
 
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: commentsNumber,
+        spaceBetween: 50,
+        navigation: {
+            nextEl: ".swiper-button-next1",
+            prevEl: ".swiper-button-prev1",
+        },
+        loop: true,
+    });
+};
 window.onscroll = function () {
     const currentScrollY = window.scrollY;
     const HeaderHeight = Header.offsetHeight;
@@ -26,13 +65,3 @@ window.onscroll = function () {
         added = false;
     }
 };
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: commentsNumber,
-    spaceBetween: 50,
-    navigation: {
-        nextEl: ".swiper-button-next1",
-        prevEl: ".swiper-button-prev1",
-    },
-    loop: true,
-});
