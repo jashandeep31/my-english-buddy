7b8153cb92de3f0540837c918d33e6445b6346c3
Jashandeep Singh <jashandeep1659@gmail.com>
2023-03-04 17:33:58 +0530

    mobile view 01

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,23 @@ const Hero = document.querySelector("#hero");
 const InnerWidth = window.innerWidth;
 const InnerHeight = window.innerHeight;
 
+let commentsNumber = 1;
 let added = false;
+window.onload = function () {
+    const HeaderHeight = Header.offsetHeight;
+    Hero.style.paddingTop = HeaderHeight + "px";
+    if (InnerWidth < 768) {
+        commentsNumber = 2;
+    }
+};
+
 window.onscroll = function () {
     const currentScrollY = window.scrollY;
     const HeaderHeight = Header.offsetHeight;
     if (currentScrollY > HeaderHeight && !added) {
         Header.style.borderBottom = "1px solid #eaeaea";
         Header.style.backgroundColor = "#fcf5ebf3";
+
         added = true;
     } else if (added && currentScrollY < HeaderHeight) {
         Header.style.borderBottom = "0px solid #eaeaea";
@@ -18,7 +28,7 @@ window.onscroll = function () {
 };
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
+    slidesPerView: commentsNumber,
     spaceBetween: 50,
     navigation: {
         nextEl: ".swiper-button-next1",
